fcf5d50b1c630370304fbb9b705c39e85858a95b
Christopher Carroll <ccarroll4812@gmail.com>
2023-08-21 13:08:44 -0400

    Fixed the Assistant import. Updates log properly

--- netnavi/src/cljc/netnavi/assistant.clj
+++ netnavi/src/cljc/netnavi/assistant.clj
@@ -1,10 +1,10 @@
-(ns netnavi.assistant
-  (:require [netnavi.plugins.gpt]))
+(ns netnavi.assistant)
 
 (defrecord Assistant [running-log tailored-prompt])
 
+
 ; This should be moved to GPT Module
-(def assistant (->Assistant (atom gpt/empty-chat) (atom gpt/empty-chat)))
+(def assistant (->Assistant (atom empty-chat) (atom empty-chat)))
 
 (defn format-prompt [prompt]
   (let [new-map {:role "user" :content prompt}]
@@ -15,20 +15,15 @@
     new-map))
 
 (defn log-prompt-update! [prompt]
-  (swap! (:running-log assistant) conj (format-prompt prompt)))
+  (swap! (:running-log assistant) conj (format-prompt prompt))
+  prompt)
 
 (defn log-response-update! [response]
-  (swap! (:running-log assistant) conj (format-response response)))
+  (swap! (:running-log assistant) conj (format-response response))
+  response)
 
 #_(let [message "This is a test"]
   (log-prompt-update! message)
   (println "Current running-log: " @(:running-log assistant))
   (log-response-update! (format-response (chat-with-assistant @(:running-log assistant)))) 
   (println "Current running-log: " @(:running-log assistant)))
-
-(println @(:running-log assistant))
-(chat-with-assistant @(:running-log assistant))
-
-(log-response-update! (format-response (chat-with-assistant "Testing")))
-
-(quick-chat-with-assistant "This is a test")
